ca50e8aebead7d67ad7130db6753a568b4461f6a
Himanshu Bobade <himanshu.bobade@roxiler.com>
2024-01-22 08:56:34 +0530

    initail setup

---
+++ story-swift/src/App.jsx
@@ -0,0 +1,17 @@
+import { Calendly, CallToAction, Footer, Home, NavBar, SampleWork, Vsl } from "./components";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Home />
+      <Vsl />
+      <CallToAction />
+      <SampleWork/>
+      <Calendly/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ story-swift/src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from "./Home";
+export { default as NavBar } from "./NavBar";
+export { default as Vsl } from "./Vsl";
+export { default as CallToAction } from "./CallToAction";
+export { default as SampleWork } from "./SampleWork";
+export { default as Calendly } from "./Calendly";
+export { default as Footer } from "./Footer";
