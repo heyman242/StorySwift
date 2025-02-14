05e3c60ea5b692fccb4b53d6088246f573c3dd3c
himanshu bobade <himanshubobade96@gmail.com>
2025-02-14 20:40:04 +0530

    feat: added google tag

--- story-swift/src/main.jsx
+++ story-swift/src/main.jsx
@@ -3,14 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import TagManager from "react-gtm-module";
-
-const tagManagerArgs = {
-  gtmId: "GTM-86EDZ173N",
-};
-
-TagManager.initialize(tagManagerArgs);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
